42de3bc878a0e32a2cbe94c1ed293a74041ab873
RAJKISHORE HUDUK <rajkishorehuduk@gmail.com>
2025-08-10 22:09:16 +0530

    Update ATM.java

--- ATM.java
+++ ATM.java
@@ -14,4 +14,18 @@ public class BankSystem {
         }
         return null;
     }
+
+    public static void main(String[] args) {
+        BankSystem bank = new BankSystem();
+
+        bank.addUser("user1", "1234");
+        bank.addUser("user2", "5678");
+
+        User loggedIn = bank.login("user1", "1234");
+        if (loggedIn != null) {
+            System.out.println("Login successful! Welcome " + loggedIn.getUserId());
+        } else {
+            System.out.println("Invalid credentials.");
+        }
+    }
 }
